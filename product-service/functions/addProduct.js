bb4e8ee740d1875bcf7656aeacba19e95b8394b4
Alexander <sannnao.developer@gmail.com>
2022-07-18 15:20:04 +0200

    feat: add relations to stocks

--- product-service/functions/addProduct.js
+++ product-service/functions/addProduct.js
@@ -7,19 +7,56 @@ module.exports.addProduct = async (event) => {
   await client.connect();
 
   try {
-    const payload = JSON.parse(event.body);
-    const { title, description, price } = payload;
+    const payload = event.body;
+    const { title, description, price, count } = payload;
 
     await client.query(
       `
-        insert into products (title, description, price)
-        values ('${title}', '${description}', '${price}')
+        create table if not exists products (
+            id uuid primary key default uuid_generate_v4(),
+            title text not null,
+            description text,
+            price integer
+        )
       `
     );
 
+    await client.query(
+      `
+        create table if not exists stocks (
+          product_id uuid,
+          count integer,
+          foreign key ("product_id") references products ("id") ON DELETE CASCADE
+        )
+      `
+    );
+
+    const createProductResult = await client.query(
+      `
+        insert into products (title, description, price)
+        values ('${title}', '${description}', '${price}')
+        returning *
+      `
+    );
+
+    const [createdProduct] = createProductResult.rows;
+
+    const createStockResult = await client.query(
+      `
+          insert into stocks (product_id, count)
+          values ('${createdProduct.id}', '${count}')
+          returning *
+        `
+    );
+
+    const [createdStock] = createStockResult.rows;
+
     return {
       statusCode: 200,
-      body: JSON.stringify("nice"),
+      body: JSON.stringify({
+        ...createdProduct,
+        count: createdStock.count,
+      }),
     };
   } catch (err) {
     console.error("Error during database request executing:", err);
